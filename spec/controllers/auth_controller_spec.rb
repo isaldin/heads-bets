adc9f4893aa80a5bae4597e68862cfaf612e0f00
isaldin <saldin.iliya@gmail.com>
2014-01-16 03:39:46 +0600

    tiny fix

--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -7,9 +7,9 @@ describe AuthController do
     controller.params[:format].should == '&access_token=4f16cb17cbb346d78be98d11f5c13b180dd06e697a2fdbc0151098aa5da849fe9a05751745e0af60f44c5&expires_in=86400&user_id=12914467'
 
     controller.parse_token(controller.params[:format]).should == {
-        :access_token => '4f16cb17cbb346d78be98d11f5c13b180dd06e697a2fdbc0151098aa5da849fe9a05751745e0af60f44c5',
-        :expires_in => '86400',
-        :user_id => '12914467'
+        access_token: '4f16cb17cbb346d78be98d11f5c13b180dd06e697a2fdbc0151098aa5da849fe9a05751745e0af60f44c5',
+        expires_in: '86400',
+        user_id: '12914467'
     }
   end
 
